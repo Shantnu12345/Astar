4d6ef7aea6d861453d259c4e2a927431750c1df9
Shantnu Kakkar <shantnukakkar1992@gmail.com>
2020-07-07 01:11:36 -0400

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "Astar.hpp"
 
 int main() {
+    Astar astarAlgo(State(10.0f, 0.0f, PI));
+
+    vector<State> path = astarAlgo.findPath(State(0.0f, 0.0f, 0.0f));
+    cout << path.size();
+
+
     std::cout << "Hello World!" << "\n";
     return 0;
 }

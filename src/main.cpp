1e2ee1af6f93f36a742fa3b49479cbfaa017307e
Shantnu Kakkar <shantnukakkar1992@gmail.com>
2020-07-07 03:46:07 -0400

    Added to_csv function

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include "Astar.hpp"
+#include <fstream>
+
+void to_csv(vector<State> const& path){
+    std::ofstream ofs("Path.csv");
+    ofs << "x,y" << std::endl;
+    for(auto const& point:path){
+        ofs << point.x << "," << point.y << endl;
+    }
+}
 
 int main() {
-    Astar astarAlgo(State(10.0f, 0.0f, PI));
-
-    vector<State> path = astarAlgo.findPath(State(0.0f, 0.0f, 0.0f));
-    cout << path.size();
-
-
-    std::cout << "Hello World!" << "\n";
+    Astar astarAlgo(State(0.0f, 0.0f, PI));
+    vector<State> path = astarAlgo.findPath(State(10.0f, 0.0f, 0.0f));
+    std::cout << "Hurray!! Found path with " << path.size() << " waypoints" << endl ;
+    to_csv(path);
     return 0;
 }
